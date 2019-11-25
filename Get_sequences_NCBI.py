228ed15cb461ac91fc807eac15cbffed57f7b12a
UGarCil <45892072+UGarCil@users.noreply.github.com>
2019-11-25 02:18:35 -0800

    Update Get_sequences_NCBI.py

--- Get_sequences_NCBI.py
+++ Get_sequences_NCBI.py
@@ -3,18 +3,20 @@
 identifiers later to download the sequences from NCBI into an specified folder'''
 
 #The name of your taxon and genes here
-gene='ND1'
-taxon='Phidippus'
+gene='18s'
+taxon='Habronattus'
 
 from Bio import Entrez
 Entrez.email='example@gmail.com'#Specify your email
-handle= Entrez.esearch(db='nucleotide', term='%s'%taxon+'[ORGN] AND %s'%gene, idtype='acc')
+handle= Entrez.esearch(db='nucleotide', retmax=100000, term='%s'%taxon+'[ORGN] AND %s'%gene, idtype='acc')
 record=Entrez.read(handle)
+print(record)
 handle.close()
 for keys, values in record.items():
     if keys=='IdList':#If the category in the dictionary contains the GIs, then put each GI in a list
         GIs=[x for x in values]
 
+print(GIs)
 
 '''this function edits the first line of each fasta file to structure the name of the file
 in the specified directory'''
@@ -33,17 +35,50 @@ def ProcName(firlin):
             finalstring+=new_firlin[k]+' '
     return (finalstring)
 
+'''####################################################################################'''
 
 '''For each of the GIs retrieved by the esearch(), download them and 
 put them into a fasta file, then export name it, then export it'''
+#for gi in GIs:
+#    handleII=Entrez.efetch(db='nuccore', id= gi, rettype= 'fasta', retmode='text')
+#    first=True
+#    for line in handleII:
+#        if first:
+#            name_file=line.split(' ')
+#            export=open(r'/home/uriel/Desktop/NCBI/test_bIOSTAR/%s'%(gene+'_'+ProcName(line)),'w')
+#            first=False
+##        print(line.strip())
+#        export.write(line)
+#    export.close()
+
+
+'''Alternatively, you can put everything in the same file, named after the 
+taxon and the gene name into a fasta file.'''
+
+'''With the sequences separated into tabs'''
+#
+#export=open(r'/home/uriel/Desktop/NCBI/test_bIOSTAR/%s_'%taxon+"%s"%gene+".fasta",'w')
+#for gi in GIs:
+#    handleII=Entrez.efetch(db='nuccore', id= gi, rettype= 'fasta', retmode='text')
+#    for line in handleII:
+#        export.write(line)
+#export.close()
+
+
+'''or as a single giant string (I recommend this one for aTRAM)'''
+
+export=open(r'/home/uriel/Desktop/NCBI/test_bIOSTAR/%s_'%taxon+"%s"%gene+".fasta",'w')
 for gi in GIs:
     handleII=Entrez.efetch(db='nuccore', id= gi, rettype= 'fasta', retmode='text')
-    first=True
+    newstring=""
+    First=True
     for line in handleII:
-        if first:
-            name_file=line.split(' ')
-            export=open(r'/home/uriel/Desktop/NCBI/test_bIOSTAR/%s'%(gene+'_'+ProcName(line)),'w')
-            first=False
-#        print(line.strip())
-        export.write(line)
-    export.close()
+#        print(line)
+        if not First:
+            newstring+=line.strip()
+        else:
+            newstring+=line.strip()+"\n"
+            First=False
+    newstring+='\n'
+    export.write(newstring)
+export.close()
